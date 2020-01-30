dc0e73fbf3e42b1d8ec3929a5d62611d7de04f46
violenil <retrospect@protonmail.com>
2020-01-30 20:48:09 +0100

    added some more parsing of xml

--- json_to_xml.py
+++ json_to_xml.py
@@ -11,5 +11,10 @@ with open('movieData.xml', 'w') as output:
         for actor in m["Characters"]:
             output.write("\t\t<actor>" + actor + "</actor>\n")
         output.write("\t</actors>\n")
+        output.write("\t<director>" + m["Director"] + "</director>\n")
+        for r in m["Release_dates"]:
+            output.write("\t<release>" + r + "</release>\n")
+        if type(m["Plot"]) == str:
+            output.write("\t<plot>\n" + m["Plot"] + "\n</plot>\n")
 
 output.close()
